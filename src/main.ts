dcfb7bf42ebd93df56472a1e4842e41413ee4fcd
Robyn Drake <robin.drake.041@gmail.com>
2022-07-05 02:48:19 -0500

    Interim commit + added dist/ to .gitignore

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import express, { Express, Request, Response } from 'express';
-import dotenv  from 'dotenf';
+import dotenv  from 'dotenv';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 65000;
 
-app.get('/', (req: Request, res:Response){
-    res.send('Express + Typescript Server');
+app.get('/', (req: Request, res:Response) => {
+    res.send('Express + Typescript Server is running');
 });
 
 app.listen('65000', () => {
